98d7a3da8f775aa56dbfcb0fcacf4a3bb0cd4bbf
William Phillips <wjphillips_git@outlook.com>
2019-10-14 16:57:04 -0700

    Add project files.

---
+++ ChampRuneDataset.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolChampionRunePageMap
+{
+    public class ChampRuneDataset
+    {
+        public string primaryTree { get; set; }
+        public string secondaryTree { get; set; }
+        public int[] primaryArray { get; set; }
+        public int[] secondaryArray { get; set; }
+        public int[] tertiaryArray { get; set; }
+        public ChampRuneDataset(string priTree, string secTree, int[] priArray, int[] secArray, int[] tertArray)
+        {
+            primaryTree = priTree;
+            secondaryTree = secTree;
+            primaryArray = priArray;
+            secondaryArray = secArray;
+            tertiaryArray = tertArray;
+        }
+    }
+}
---
+++ ChampRuneMapping.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolChampionRunePageMap
+{
+    public class ChampRuneMapping : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> PageName { get; set; }
+
+        [Category("Output")]
+        public OutArgument<ChampRuneDataset> RuneData { get; set; }
+        private Dictionary<string, ChampRuneDataset> GetChampRuneDataMatrix = new Dictionary<string, ChampRuneDataset>
+        {
+            {"XAYAH", new ChampRuneDataset("PRECISION", "INSPIRATION", new int[4]{2, 2, 1, 1}, new int[3]{2, 3, 0 }, new int[3] {2, 1, 2 })},
+            {"TRISTANA", new ChampRuneDataset("PRECISION", "INSPIRATION", new int[4]{1, 2, 3, 1}, new int[3]{2, 3, 0 }, new int[3] {2, 1, 2 })},
+            {"VAYNE", new ChampRuneDataset("PRECISION", "INSPIRATION", new int[4]{1, 2, 1, 1}, new int[3]{2, 3, 0 }, new int[3] {2, 1, 2 })},
+            {"VAYNE2", new ChampRuneDataset("PRECISION", "SORCERY", new int[4]{1, 2, 1, 1}, new int[3]{0, 3, 3 }, new int[3] {2, 1, 2 })},
+            {"ASHE", new ChampRuneDataset("PRECISION", "INSPIRATION", new int[4]{2, 2, 1, 1}, new int[3]{2, 3, 0 }, new int[3] {2, 1, 2 })},
+            {"KAYN", new ChampRuneDataset("DOMINATION", "SORCERY", new int[4]{1, 3, 3, 1}, new int[3]{0, 2, 3 }, new int[3] {1, 1, 2 })},
+            {"NAUTILUS", new ChampRuneDataset("RESOLVE", "INSPIRATION", new int[4]{2, 1, 3, 3}, new int[3]{0, 3, 1 }, new int[3] {3, 2, 1 })},
+            {"THRESH", new ChampRuneDataset("RESOLVE", "INSPIRATION", new int[4]{2, 2, 3, 1}, new int[3]{0, 3, 1 }, new int[3] {3, 2, 1 })},
+            {"NAMI", new ChampRuneDataset("SORCERY", "INSPIRATION", new int[4]{1, 2, 1, 1}, new int[3]{0, 3, 1 }, new int[3] {1, 1, 2 })}
+        };
+        
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var pageName = PageName.Get(context).ToUpper();
+            RuneData.Set(context, GetChampRuneDataMatrix[pageName]);
+        }
+    }
+}
